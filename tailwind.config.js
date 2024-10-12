7a0d79e2c166ebaf24e048364b10e2790df26ddb
IlhamSoejudAlkahfiardy <ilhamyusuf386@gmail.com>
2024-10-12 10:07:35 +0700

    style: change gradient hero text colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,8 @@ export default {
         lightBlack: "#181818",
         black: "#090909", // Warna hitam
         hoverBlack: "#2F2F2F", // Warna hitam
-        // lightGold: "#ca8a04",
         lightGold: "#EBBE87",
-        // gold: "#854d0e", // Warna emas
         gold: "#B28752", // Warna emas
-        // hoverGold: "#713f12",
         hoverGold: "#B77B32",
         gradientText: {
           black: "rgb(202, 138, 4)", // Warna dasar
